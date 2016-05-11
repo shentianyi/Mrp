d5b8629bef16f07165d4f2b2228f98a8ce3304ef
Charlot <7wangsong@gmail.com>
2016-05-11 14:44:19 +0800

    on wms plugin

---
+++ WMSPlugIn/Service/StorageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KskPlugInSharedObject;
+
+namespace WMSPlugIn.Service
+{
+    public class StorageService : IStorageService
+    {
+        public ProcessResult GetPartsInventory(List<string> PartNr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
